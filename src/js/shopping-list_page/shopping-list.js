38f45a6c1c3870fcfb0bbdf9440fe3ab84932e5c
natalia130 <144233915+natalia130@users.noreply.github.com>
2024-03-02 21:05:19 +0200

    shopping list marckup 1

--- src/js/shopping-list_page/shopping-list.js
+++ src/js/shopping-list_page/shopping-list.js
@@ -1,9 +1,60 @@
 import { getBookById } from '../home_page/books-api';
 
-const addButton = document.querySelector('.add-button-sh'); 
- 
-addButton.addEventListener('click', handleAddBtnClick); 
- 
-async function handleAddBtnClick(event) { 
-    const data = await getBookById('643282b2e85766588626a114');
+const refs = {
+    shopListEmptElem: document.querySelector(".shop-list-empty"),
+    shopListDivElem: document.querySelector(".shopping-list"),
 }
+
+const bookIdList = ["643282b1e85766588626a081", "643282b1e85766588626a0b2", "643282b2e85766588626a112"];//замінити потім
+renderShopList(bookIdList);
+
+async function renderShopList(bookIdList) {
+    if (bookIdList.length === 0) {
+        refs.shopListEmptElem.classList.remove("is-hidden")
+    } else {
+        refs.shopListEmptElem.classList.add("is-hidden")
+        const markup = await shopListTemplate(bookIdList);
+        refs.shopListDivElem.insertAdjacentHTML("beforeend", `<ul>${markup}</ul>`);
+    }
+}
+
+async function shopListTemplate(books) {
+    const bookTemplates = await Promise.all(books.map(bookTemplate));
+    return bookTemplates.join("");
+}
+
+async function bookTemplate(book) {
+    const bookData = await getBookById(book);
+    const { book_image, title, list_name, description, author, amazon_product_url, buy_links } = bookData;
+
+    return `<li class="shopping-list-item">
+        <img class="shopping-list-image"
+            src="${book_image}"
+            alt="${title}" />
+        <div class="book-info">
+            <div>
+                <div>
+                    <h1>${title}</h1>
+                    <p>${list_name}</p>
+                </div>
+                <button class="shopping-list-delete-btn"></button>
+            </div>
+        <p>${description}</p>
+        <div>
+        <p>${author}</p>
+        <ul>
+        <li><a href="${amazon_product_url}" target="_blank>amazon</a></li>
+        <li><a href="${buy_links[1].url}" target="_blank>applebooks</a></li>
+        </ul>
+        </div>
+        </div>
+    </li>`;
+}
+
+// const addButton = document.querySelector('.add-button-sh'); 
+ 
+// addButton.addEventListener('click', handleAddBtnClick); 
+ 
+// async function handleAddBtnClick(event) { 
+//     const data = await getBookById('643282b2e85766588626a114');
+// }
