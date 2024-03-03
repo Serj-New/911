8e578fe94124c7ed09a7b6026ffc21137382a303
Sergey Novikov <69253755+Serj-New@users.noreply.github.com>
2024-03-03 23:52:50 +0200

    Update partners.js

--- src/js/partners.js
+++ src/js/partners.js
@@ -2,47 +2,47 @@ const charityFunds = [
   {
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: 'img/funds/save-the-children.png',
+    img: '../img/funds/save-the-children.png',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: 'img/funds/project-hope.png',
+    img: '../img/funds/project-hope.png',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: 'img/funds/united24.png',
+    img: '../img/funds/united24.png',
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: 'img/funds/international-medical-corps.png',
+    img: '../img/funds/international-medical-corps.png',
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: 'img/funds/medicines-sans-frontieres.png',
+    img: '../img/funds/medicines-sans-frontieres.png',
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: 'img/funds/razom.png',
+    img: '../img/funds/razom.png',
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: 'img/funds/action-against-hunger.png',
+    img: '../img/funds/action-against-hunger.png',
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: 'img/funds/world-vision.png',
+    img: '../img/funds/world-vision.png',
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: 'img/funds/sergiy-prytula.png',
+    img: '../img/funds/sergiy-prytula.png',
   },
 ];
 
