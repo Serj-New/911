981816c269e1f0eed745c07183b9f2ab7e4d71df
natalia130 <144233915+natalia130@users.noreply.github.com>
2024-03-04 23:42:07 +0200

    fixed shopping list js and css, added delete btn

--- src/js/shopping-list_page/shopping-list.js
+++ src/js/shopping-list_page/shopping-list.js
@@ -1,40 +1,58 @@
-import { getBookById } from '../home_page/books-api';
+import { handleAddBtnClick, handleGetBtnClick, handleDelBtnClick } from '../shopping-list_page/local-storage'; // видалити handleAddBtnClick
 
 const refs = {
     shopListEmptElem: document.querySelector(".shop-list-empty"),
     shopListDivElem: document.querySelector(".shopping-list"),
+    shopListElem: document.querySelector(".shop-list"),
     pageHeader: document.querySelector(".header-home"),
 }
 
-const bookIdList = ["643282b1e85766588626a081", "643282b1e85766588626a0b2", "643282b2e85766588626a112"];//замінити потім
-renderShopList(bookIdList);
+handleAddBtnClick("643282b1e85766588626a081"); // видалити
+handleAddBtnClick("643282b1e85766588626a0b2"); //
+handleAddBtnClick("643282b2e85766588626a112"); //
+handleAddBtnClick("643282b1e85766588626a0d4"); //
 
-async function renderShopList(bookIdList) {
-    if (bookIdList.length === 0) {
-        refs.shopListEmptElem.classList.remove("is-hidden")
+
+const booksList = handleGetBtnClick();
+renderShopList(booksList);
+
+function renderShopList(booksList) {
+    if (booksList.length === 0) {
+        refs.shopListEmptElem.classList.remove("is-hidden");
     } else {
         refs.shopListEmptElem.classList.add("is-hidden");
-        const markup = await shopListTemplate(bookIdList);
-        refs.shopListDivElem.insertAdjacentHTML("beforeend", `<ul class="shop-list">${markup}</ul>`);
+        const markup = shopListTemplate(booksList);
+        refs.shopListElem.insertAdjacentHTML("beforeend", markup);
+    }
+
+    const deleteBtnElements = document.querySelectorAll(".shop-list-delete-btn");
+    deleteBtnElements.forEach(button => {
+        button.addEventListener("click", onDeleteBtnClick);
+    });
+
+    function onDeleteBtnClick(event) {
+        const id = event.target.closest(".shop-list-item").id;
+        handleDelBtnClick(id);
+        const bookToRemove = document.getElementById(id);
+        bookToRemove.parentNode.removeChild(bookToRemove);
+        if (refs.shopListElem.childElementCount === 0) {
+            refs.shopListEmptElem.classList.remove("is-hidden");
+        }
+        
     }
 }
 
-async function shopListTemplate(books) {
-    const bookTemplates = await Promise.all(books.map(bookTemplate));
-    return bookTemplates.join("");
+function shopListTemplate(books) {
+    return books.map(bookTemplate).join("");
 }
 
-async function bookTemplate(book) {
-    const bookData = await getBookById(book);
-    const { book_image, title, list_name, description, author, amazon_product_url, buy_links } = bookData;
+function bookTemplate(book) {
+    const { _id, book_image, title, list_name, description, author, amazon_product_url, buy_links } = book;
 
-    return `<li class="shop-list-item">
+    return `<li class="shop-list-item" id="${_id}">
         <img class="shop-list-img"
             src="${book_image}"
-            alt="${title}" />
-        <button class="shop-list-delete-btn"><svg class="delete-btn-icon">
-                  <use href="./img/icons.svg#icon-trash"></use>
-                </svg></button>   
+            alt="${title}" />  
         <div class="book-info">
         <h1 class="book-title">${title}</h1>
         <p class="book-category">${list_name}</p>
@@ -44,22 +62,25 @@ async function bookTemplate(book) {
         <ul class="book-shop-links">
         <li><a href="${amazon_product_url}" target="_blank"><img class="book-shop-img amazon-logo"
         srcset="
-        ./img/internet-shops/amazon@1x.png 1x,
-        ./img/internet-shops/amazon@2x.png 2x" 
-        src="./img/internet-shops/amazon@1x.png"
+        /img/internet-shops/amazon@1x.png 1x,
+        /img/internet-shops/amazon@2x.png 2x" 
+        src="/img/internet-shops/amazon@1x.png"
         alt="Amazon logo"
         width="32"
         height="11"/></a></li>
         <li><a class="book-shop-link" href="${buy_links[1].url}" target="_blank"><img class="book-shop-img apple-books-logo"
         srcset="
-        ./img/internet-shops/book@1x.png 1x,
-        ./img/internet-shops/book@2x.png 2x" 
-        src="./img/internet-shops/book@1x.png"
+        /img/internet-shops/book@1x.png 1x,
+        /img/internet-shops/book@2x.png 2x" 
+        src="/img/internet-shops/book@1x.png"
         alt="Apple Books logo"
         width="16"
         height="16"/></a></li>
         </ul>
         </div>
+        <button class="shop-list-delete-btn"><svg class="delete-btn-icon">
+                  <use href="/img/icons.svg#icon-trash"></use>
+                </svg></button> 
         </div>
     </li>`;
 }
