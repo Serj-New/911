cd62dfe0a58622a32cc03039c9008177a1e042fa
Serj-New <69253755+Serj-New@users.noreply.github.com>
2024-03-08 12:22:38 +0000

    Deploying to gh-pages from @ Serj-New/911@a48470d71d91c0372342f094b0d74d51652f5174 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,21 +1,21 @@
-import{h as l,e as u}from"./assets/local-storage-22d1bda9.js";import{P as v}from"./assets/vendor-bfab3ab7.js";const o={shopListEmptElem:document.querySelector(".shop-list-empty"),shopListElem:document.querySelector(".shop-list"),paginationElem:document.getElementById("pagination"),pageHeader:document.querySelector(".header-home")},n=new v("pagination",{totalItems:l().length,itemsPerPage:window.innerWidth<768?4:3,visiblePages:2,centerAlign:!0});r();window.addEventListener("resize",k);function k(){const e=n.getCurrentPage();n.movePageTo(e)}n.on("afterMove",e=>{const t=window.innerWidth<768?4:3;n.setItemsPerPage(t),r()});function r(){if(d()){const e=n.getCurrentPage(),t=window.innerWidth<768?4:3,s=(e-1)*t,i=s+t,a=l().slice(s,i);E(a)}}function E(e){document.querySelectorAll(".shop-list-delete-btn").forEach(a=>{a.removeEventListener("click",c)});const s=b(e);o.shopListElem.innerHTML=s,document.querySelectorAll(".shop-list-delete-btn").forEach(a=>{a.addEventListener("click",c)})}function d(){return l().length===0?(o.shopListElem.classList.add("is-hidden"),o.paginationElem.classList.add("is-hidden"),o.shopListEmptElem.classList.remove("is-hidden"),!1):(o.shopListElem.classList.remove("is-hidden"),o.shopListEmptElem.classList.add("is-hidden"),o.paginationElem.classList.remove("is-hidden"),!0)}function c(e){const t=e.target.closest(".shop-list-item").id;u(t);const s=document.getElementById(t);s.parentNode.removeChild(s);const i=n.getCurrentPage();n.reset(l().length),r(),n.movePageTo(i),d()}function b(e){return e.map(f).join("")}function f(e){const{_id:t,book_image:s,title:i,list_name:a,description:m,author:g,amazon_product_url:p,buy_links:h}=e;return`<li class="shop-list-item" id="${t}">
+import{h as l,e as k,i as c}from"./assets/local-storage-42c70929.js";import{P as v}from"./assets/vendor-bfab3ab7.js";const E="/911/assets/icons-2409f238.svg",o={shopListEmptElem:document.querySelector(".shop-list-empty"),shopListElem:document.querySelector(".shop-list"),paginationElem:document.getElementById("pagination"),pageHeader:document.querySelector(".header-home")},n=new v("pagination",{totalItems:l().length,itemsPerPage:window.innerWidth<768?4:3,visiblePages:2,centerAlign:!0});r();window.addEventListener("resize",b);function b(){const e=n.getCurrentPage();n.movePageTo(e)}n.on("afterMove",e=>{const t=window.innerWidth<768?4:3;n.setItemsPerPage(t),r()});function r(){if(m()){const e=n.getCurrentPage(),t=window.innerWidth<768?4:3,s=(e-1)*t,i=s+t,a=l().slice(s,i);f(a)}}function f(e){document.querySelectorAll(".shop-list-delete-btn").forEach(a=>{a.removeEventListener("click",d)});const s=L(e);o.shopListElem.innerHTML=s,document.querySelectorAll(".shop-list-delete-btn").forEach(a=>{a.addEventListener("click",d)})}function m(){return l().length===0?(o.shopListElem.classList.add("is-hidden"),o.paginationElem.classList.add("is-hidden"),o.shopListEmptElem.classList.remove("is-hidden"),!1):(o.shopListElem.classList.remove("is-hidden"),o.shopListEmptElem.classList.add("is-hidden"),o.paginationElem.classList.remove("is-hidden"),!0)}function d(e){const t=e.target.closest(".shop-list-item").id;k(t);const s=document.getElementById(t);s.parentNode.removeChild(s);const i=n.getCurrentPage();n.reset(l().length),r(),n.movePageTo(i),m()}function L(e){return e.map(P).join("")}function P(e){const{_id:t,book_image:s,title:i,list_name:a,description:p,author:h,amazon_product_url:g,buy_links:u}=e;return`<li class="shop-list-item" id="${t}">
         <img class="shop-list-img"
             src="${s}"
             alt="${i}" />  
         <div class="book-info">
         <h1 class="book-title">${i}</h1>
         <p class="book-category">${a}</p>
-        <p class="book-description">${m}</p>
+        <p class="book-description">${p}</p>
         <div class="card-footer">
-        <p class="book-author">${g}</p>
+        <p class="book-author">${h}</p>
         <ul class="book-shop-links">
-        <li><a href="${p}" target="_blank"><svg class="book-shop-img amazon-logo"><use href="./img/sprite.svg#icon-amazon"></use></svg></a></li>
-        <li><a class="book-shop-link" href="${h[1].url}" target="_blank"><svg class="book-shop-img apple-books-logo"><use href="./img/sprite.svg#icon-ibooks"></use></svg></a></li>
+        <li><a href="${g}" target="_blank"><svg class="book-shop-img amazon-logo"><use href="${c}#icon-amazon"></use></svg></a></li>
+        <li><a class="book-shop-link" href="${u[1].url}" target="_blank"><svg class="book-shop-img apple-books-logo"><use href="${c}#icon-ibooks"></use></svg></a></li>
         </ul>
         </div> 
         </div>
         <button class="shop-list-delete-btn"><svg class="delete-btn-icon">
-                  <use href="./img/icons.svg#icon-trash"></use>
+                  <use href="${E}#icon-trash"></use>
                 </svg></button>
     </li>`}window.addEventListener("DOMContentLoaded",e=>{o.pageHeader.dataset.pageName="shopping-list"});
 //# sourceMappingURL=commonHelpers2.js.map
