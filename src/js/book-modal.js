a4dd0cb339a9a6533b6ba919f3e4731c488ecb39
Sergey Novikov <69253755+Serj-New@users.noreply.github.com>
2024-03-08 17:16:50 +0200

    last upd

--- src/js/book-modal.js
+++ src/js/book-modal.js
@@ -62,67 +62,66 @@ function closeModal(evt) {
 const parentElem = document.querySelector('.main-page-right');
 parentElem.addEventListener('click', onClick);
 
-function onClick(evt) {
-  const bookCard = evt.target.parentNode;
-  const bookCardId = bookCard.dataset.id;
-
-  let basketArr = handleGetBtnClick();
-
-  getBookById(bookCardId)
-    .then(obj => {
-      if (bookCard.classList.contains('js-book-card')) {
-        createMarkup(obj);
-      }
-
-      const addBook = document.querySelector('#js-book-modal-btn');
-      const inStorage = basketArr.some(({ _id }) => _id === obj._id);
-
-      document.querySelector('.book-modal-buy').style.display = 'none';
-
-      if (inStorage) {
-        addBook.addEventListener('click', removeFromCart);
-        addBook.classList.remove('js-add');
-        addBook.classList.add('js-remove');
-        document.querySelector('.book-modal-buy').style.display = 'block';
-      } else {
-        addBook.addEventListener('click', addToCart);
-        addBook.classList.add('js-add');
-        addBook.classList.remove('js-remove');
-        document.querySelector('.book-modal-buy').style.display = 'none';
-      }
-
-      function addToCart(evt) {
-        const btnAdd = evt.target.classList.contains('js-add');
-        const inStorage = basketArr.some(({ _id }) => _id === obj._id);
-
-        if (btnAdd) {
-          evt.target.classList.remove('js-add');
-          evt.target.classList.add('js-remove');
-          document.querySelector('.book-modal-buy').style.display = 'block';
-
-          if (inStorage) {
-            return;
-          }
-
-          handleAddBtnClick(obj._id);
-          basketArr = handleGetBtnClick();
-        }
-      }
-
-      function removeFromCart(evt) {
-        const btnRemove = evt.target.classList.contains('js-remove');
-
-        if (btnRemove) {
-          evt.target.classList.remove('js-remove');
-          evt.target.classList.add('js-add');
-          document.querySelector('.book-modal-buy').style.display = 'none';
-
-          handleDelBtnClick(obj._id);
-          basketArr = handleGetBtnClick();
-        }
-      }
-    })
-    .catch(err => console.log(err));
+function onClick(evt) { 
+  const bookCard = evt.target.parentNode; 
+  const bookCardId = bookCard.dataset.id; 
+ 
+  let basketArr = handleGetBtnClick(); 
+ 
+  getBookById(bookCardId) 
+    .then(obj => { 
+      if (bookCard.classList.contains('js-book-card')) { 
+        createMarkup(obj); 
+      } 
+ 
+      const addBook = document.querySelector('#js-book-modal-btn'); 
+      const inStorage = basketArr.some(({ _id }) => _id === obj._id); 
+ 
+      document.querySelector('.book-modal-buy').style.display = 'none'; 
+ 
+      if (inStorage) { 
+        handleAddBook(); 
+      } else { 
+        handleDelBook(); 
+      } 
+ 
+      function handleAddBook() { 
+        addBook.addEventListener('click', removeFromCart); 
+        addBook.removeEventListener('click', addToCart); 
+        addBook.classList.remove('js-add'); 
+        addBook.classList.add('js-remove'); 
+        document.querySelector('.book-modal-buy').style.display = 'block'; 
+      } 
+ 
+      function handleDelBook() { 
+        addBook.addEventListener('click', addToCart); 
+        addBook.removeEventListener('click', removeFromCart); 
+        addBook.classList.add('js-add'); 
+        addBook.classList.remove('js-remove'); 
+        document.querySelector('.book-modal-buy').style.display = 'none'; 
+      } 
+ 
+      function addToCart() { 
+        handleAddBook(); 
+ 
+        const inStorage = basketArr.some(({ _id }) => _id === obj._id); 
+ 
+        if (inStorage) { 
+          return; 
+        } 
+ 
+        handleAddBtnClick(obj._id); 
+        basketArr = handleGetBtnClick(); 
+      } 
+ 
+      function removeFromCart() { 
+        handleDelBook(); 
+ 
+        handleDelBtnClick(obj._id); 
+        basketArr = handleGetBtnClick(); 
+      } 
+    }) 
+    .catch(err => console.log(err)); 
 }
 
 export { createMarkup, onClick };
